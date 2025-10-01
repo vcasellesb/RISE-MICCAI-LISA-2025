910529c7122b36276fa34e63638a60a971b2e586
Vicent Caselles <vcasellesb@uoc.edu>
2025-10-01 15:14:58 +0200

    changed way to specify dataset to download from synapse

--- src/synapse_download.py
+++ src/synapse_download.py
@@ -4,10 +4,10 @@ import synapseclient
 import synapseutils
 
 
-_ACCEPTED_SYN_IDS = [
-    'syn68646516',  # Task 2 - Training
-    'syn68737427' # Task 2 - Validation
-]
+_ACCEPTED_SYN_IDS = {
+    'training': 'syn68646516',  # Task 2 - Training
+    'validation': 'syn68737427' # Task 2 - Validation
+}
 
 
 def get_token(token_file: str):
@@ -29,12 +29,16 @@ def main(output_folder: str, synID: str, token_file: str):
 def entrypoint():
     parser = argparse.ArgumentParser()
     parser.add_argument('output_folder', type=str)
-    parser.add_argument('-synid', '--synapse_id', type=str, choices=_ACCEPTED_SYN_IDS, default='syn68646516')
+
+    parser.add_argument('-synid', '--synapse_id', type=str,
+                        choices=_ACCEPTED_SYN_IDS.keys(),
+                        default='training')
+
     parser.add_argument('--token_file', type=str, default='synapse_token')
 
     args = parser.parse_args()
 
-    main(args.output_folder, args.synapse_id, args.token_file)
+    main(args.output_folder, _ACCEPTED_SYN_IDS[args.synapse_id], args.token_file)
 
 
 if __name__ == "__main__":
